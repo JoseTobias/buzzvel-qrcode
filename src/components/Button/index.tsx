93ecaa6ef12cc021096fc632ac423e673aebb9c9
JoseTobias <tobiassouza0@gmail.com>
2023-03-19 17:04:57 -0300

    style: add styled button

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles.css";
+
+function Button() {
+  return (
+    <div>
+      <button>Generate Image</button>
+    </div>
+  );
+}
+
+export default Button;
