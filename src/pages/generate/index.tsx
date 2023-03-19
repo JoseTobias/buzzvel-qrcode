e395cfc3a8f8df71c7b9e89b22669a4e12c3c197
JoseTobias <tobiassouza0@gmail.com>
2023-03-19 17:44:22 -0300

    chore: add qr code generator

--- src/pages/generate/index.tsx
+++ src/pages/generate/index.tsx
@@ -1,10 +1,14 @@
 import React from "react";
+import QRCode from "qrcode.react";
+
+import "./styles.css";
+
 import Button from "../../components/Button";
 import Input from "../../components/Input";
 
 function GeneratePage() {
   return (
-    <>
+    <div className="page">
       <h1>QR Code Image Generator</h1>
       <div className="mb-4">
         <Input label="Name" id="name" />
@@ -16,7 +20,10 @@ function GeneratePage() {
         <Input label="Github URL" id="github" />
       </div>
       <Button />
-    </>
+      <div className="qrcode-content">
+        <QRCode value="https://reactjs.org/" renderAs="canvas" />
+      </div>
+    </div>
   );
 }
 
